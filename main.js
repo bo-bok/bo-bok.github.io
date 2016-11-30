f57a6de44c9f241dedd667c55cd62bd03ccf74ec
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-30 19:29:25 +0000

    user can exit modal by clicking on cross icon

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ var projectModal = document.getElementById('projectModal');
 // Get the button that opens the modal
 var btn = document.getElementById("projectModalBtn");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modalClose")[0];
+
+
 // When the user clicks the button, open the modal
 btn.onclick = function() {
     projectModal.style.display = "block";
+}
+
+// When the user clicks on cross icon, close the modal
+span.onclick = function() {
+    projectModal.style.display = "none";
 }
