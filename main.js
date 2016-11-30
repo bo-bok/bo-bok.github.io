0fe78ecad4708d8f082fbb3494c89899ce34a122
Jessica Salmon <jessicasalmon05@gmail.com>
2016-11-30 18:54:08 +0000

    open modal on 'project info' button click

---
+++ main.js
@@ -0,0 +1,10 @@
+// Get the modal
+var projectModal = document.getElementById('projectModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("projectModalBtn");
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    projectModal.style.display = "block";
+}
